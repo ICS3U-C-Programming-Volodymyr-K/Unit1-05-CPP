50edd3fb4f311b73c00b7ca388d56e17d88893c5
Volodymyr Kryzhanovskyi <volodymyr.kryzhnovskyi@stu.ocsb.ca>
2025-02-19 17:58:22 +0000

    Made areaPerRect.cpp

---
+++ areaPerRect.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2025 Volodymyr Kryzhanovskyi All rights reserved.
+//
+// Created by: Volodymyr Kryzhanovskyi
+// Date: 02 19, 2025
+// It calculates perimeter and area of rectangle..
+
+
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "The area of rectangle is 7*5=" << 7*5 << std::endl;
+    std::cout << "The perimeter of rectangle is 2(7+5)=" <<
+        2*7+2*5 << std::endl;
+}
